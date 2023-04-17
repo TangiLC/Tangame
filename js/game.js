bc7c64df568ed8c3a298ac0b517a3e314141f897
TangiLC <tangi@le-cadre.net>
2023-04-17 10:27:49 +0200

    bomb

--- js/game.js
+++ js/game.js
@@ -1,10 +1,12 @@
-var level=4;
-var score=0;
-var hiScore=0;
-var altitude=50;
-var leftPos=-1;
-var bombX=0;
-var bombAltitude=50;
+let level=1;
+let score=0;
+let hiScore=0;
+let altitude=500;
+let leftPos=-1;
+let nbBomb=0;
+let bombX=0;
+let bombAltitude=550;
+let planeSpeed=2;
 
 const DOMLevel=document.getElementById('score');
 const DOMScore=document.getElementById('level');
@@ -13,9 +15,7 @@ const DOMAlti=document.getElementById('alti');
 const DOMAvion=document.getElementById('avion');
 const DOMBomb=document.getElementById('bomb');
 const DOMFenetre=document.getElementById('fenetre');
-const dropingBomb=document.addEventListener('keyup', ev => {
-    if (ev.code === 'Space') {dropBomb(altitude,leftPos)}
-  });
+
 
 const buildingCol=['#0000AA','#00AA00','#00AAAA','#AA0000','#AA0000','#AA00AA','#AA5500','#FFFF55',
                    '#AAAAAA','#555555','#5555FF','#55FF55','#55FFFF','#FF5555','#FF55FF','#FFFFFF'];
@@ -24,11 +24,11 @@ const buildingCol=['#0000AA','#00AA00','#00AAAA','#AA0000','#AA0000','#AA00AA','
    06.lowYell =#AA5500 ; 07.lowGrey =#AAAAAA ; 08.higGrey =#555555 ;
    09.higBlue =#5555FF ; 10.higGren =#55FF55 ; 11.higCyan =#55FFFF ;
    12.hig_Red =#FF5555 ; 13.higMagt =#FF55FF ; 14.higYell =#FFFF55 ;*/
-var buildings=[];
+let buildings=[];
 
 function initBuildings(level,buildings){
     let numberOfBld = 6+3*level;
-    let widthOfBld = (100/(numberOfBld));
+    let widthOfBld = (99/(numberOfBld));
     for (let i=0; i<numberOfBld; i++){
         let colorIndex=Math.floor(Math.random()*15);
         let buildingHeight=50+Math.floor(Math.random()*280);
@@ -38,10 +38,25 @@ function initBuildings(level,buildings){
     drawBuildings(buildings,widthOfBld);
 }
 
+function ready(){
+    planeSpeed=Math.floor(2+level*.3);
+    let divStartMssg=document.createElement('div');
+    divStartMssg.setAttribute('class','startMessage');
+    divStartMssg.innerText=`LEVEL ${level} : PRESS ANY KEY TO START`;
+    DOMFenetre.appendChild(divStartMssg);
+    const letsStart=document.addEventListener('keyup',ev => {
+        if (ev.code === 'Space') {
+            divStartMssg.innerText="";
+            mainGame();}
+        });
+}
+
+
 function drawBuildings(buildings,widthOfBld){
     for(let i=0;i<buildings.length;i++){
         let divBldg =document.createElement('div');
         divBldg.setAttribute('class','building');
+        divBldg.setAttribute('id',`bd_${i}`);
         divBldg.style.height=`${buildings[i].bdHeight}px`;
         divBldg.style.left=`${i*widthOfBld}%`;
         divBldg.style.width=`${widthOfBld}%`;
@@ -52,12 +67,13 @@ function drawBuildings(buildings,widthOfBld){
 }
 
 function dropBomb(alti,lefti){
-    if (DOMBomb.innerText==""){
-    bombAltitude=520-10*alti; console.log(500-10*altitude,bombAltitude);
+    if (DOMBomb.innerHTML==""){
+    nbBomb+=1;
+    bombAltitude=alti; console.log('bomb:',bombAltitude)
     bombX=lefti;
-    DOMBomb.style.top=`${bombAltitude}px`;
+    DOMBomb.style.bottom=`${bombAltitude}px`;
     DOMBomb.style.left=`${bombX}%`;
-    DOMBomb.innerText="*";}
+    DOMBomb.innerHTML="<img src='./img/torpedo.bmp' width='30px' height='auto'>";}
 }
 
 function affichScore(){
@@ -66,17 +82,53 @@ function affichScore(){
     DOMHiScore.innerText="*High "+hiScore.toString().padStart(5,0);
     DOMAlti.innerText="Altitude "+(altitude*10).toString().padStart(5,0);
 }
-function moveBomb(bombAlti){
-    
-    if (bombAlti<600 && DOMBomb.innerText!=""){
-        DOMBomb.style.top=`${bombAlti}px`;
+
+const timer = ms => new Promise(res => setTimeout(res, ms))
+
+async function kaboom(myIndex){
+    let boomcolor1='#FF0000'; let boomcolor2='#FFFFFF';
+    let theBuilding=document.getElementById(`bd_${myIndex}`);
+    for(let i=0;i<4;i++){
+        theBuilding.style.backgroundColor=`${boomcolor1}`;
+        await timer(80);
+        theBuilding.style.backgroundColor=`${boomcolor2}`;
+        await timer(80);
     }
-    else {DOMBomb.innerText=""}
+    theBuilding.style.backgroundColor=`${buildings[myIndex].bdCol}`
+    theBuilding.innerText=buildings[myIndex].bdHeight;
+    theBuilding.style.height=`${buildings[myIndex].bdHeight}px`;
+
+}
+
+function explodeBuilding(bdIndex){
+    buildings[bdIndex].bdHeight-=20;
+    if (buildings[bdIndex].bdHeight<=0){buildings[bdIndex].bdHeight=0}
+    //DOMBomb.innerText="";
+    document.getElementById(`bd_${bdIndex}`).style.height=buildings[bdIndex].bdHeight;
+    kaboom(bdIndex);
+    DOMBomb.innerHTML="";
+
+
+}
+
+function moveBomb(bombAlti){
+    let bdIndex=Math.floor(bombX/(99/(6+3*level)));
+    if (bdIndex<0){bdIndex=0}
+    console.log(bombAlti,bombX,bdIndex,buildings[bdIndex].bdHeight);
+    if (bombAlti>buildings[bdIndex].bdHeight && DOMBomb.innerHTML!="" && bombAlti>0){
+        DOMBomb.style.bottom=`${bombAlti}px`;
+        DOMBomb.innerHTML="<img src='./img/torpedo.bmp' width='30px' height='auto'>";
+
+    }
+    else if (bombAlti<=buildings[bdIndex].bdHeight && DOMBomb.innerHTML!=""){
+        DOMBomb.innerHTML="<img src='./img/torpedo.bmp' width='50px' height='auto'>";
+        bombAltitude=550;
+        explodeBuilding(bdIndex);}
 }
 
 function movePlane(alti,bombAlti){
     DOMAvion.innerHTML= "";
-    DOMAvion.style.top=`${alti}px`;
+    DOMAvion.style.bottom=`${alti}px`;
     DOMAvion.style.left=`${leftPos}%`;
     DOMAvion.innerHTML=`<img src='./img/spritePlane.gif' width='80px' height='auto'>`;
     affichScore();
@@ -84,18 +136,21 @@ function movePlane(alti,bombAlti){
 }
 
 function mainGame(){
+    const dropingBomb=document.addEventListener('keyup', ev => {
+        if (ev.code === 'Space') {dropBomb(altitude,leftPos)}
+      });
     let flight= setInterval(function() {
-        let newAltitude=500-10*altitude;
-        movePlane(newAltitude,bombAltitude);
-        leftPos +=2; bombAltitude+=20;
-        if (leftPos>=96){leftPos=-1;altitude -=5;}
+        movePlane(altitude,bombAltitude);
+        leftPos +=planeSpeed; console.log('speed',planeSpeed);
+        if (bombAltitude<550){bombAltitude-=30};
+        if (leftPos>=97){leftPos=-1;altitude -=25;}
         if (altitude <0){DOMAvion.innerHTML=`<img src='./img/spritePlane.gif' height='60px' width='auto'><p>END</p>`;
            clearInterval(flight);}
-        }, 100);
+        }, 300);
     
      
     
 }  
 
 initBuildings(level,buildings);
-mainGame();
+ready();
