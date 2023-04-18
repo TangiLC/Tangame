72ee4a32140a8a8515fd368a7640bc845b7048ad
TangiLC <tangi@le-cadre.net>
2023-04-18 14:02:44 +0200

    final

---
+++ js/buildings.js
@@ -0,0 +1,62 @@
+const buildingCol=['#0000AA','#00AA00','#00AAAA','#AA0000','#AA0000','#AA00AA','#AA5500','#FFFF55',
+                   '#AAAAAA','#555555','#5555FF','#55FF55','#55FFFF','#FF5555','#FF55FF','#FFFFFF'];
+/* 00.loBlack =#000000 ; 01.lowBlue =#0000AA ; 02.lowGren =#00AA00 ;
+   03.lowCyan =#00AAAA ; 04.low_Red =#AA0000 ; 05.lowMagt =#AA00AA ;
+   06.lowYell =#AA5500 ; 07.lowGrey =#AAAAAA ; 08.higGrey =#555555 ;
+   09.higBlue =#5555FF ; 10.higGren =#55FF55 ; 11.higCyan =#55FFFF ;
+   12.hig_Red =#FF5555 ; 13.higMagt =#FF55FF ; 14.higYell =#FFFF55 ;*/
+
+
+function initBuildings(level,buildings){       //création aléatoire des buildings (hauteur et couleur)
+    let numberOfBld = 6+2*level;
+    let widthOfBld = (99/(numberOfBld));
+    for (let i=0; i<numberOfBld; i++){
+        let colorIndex=Math.floor(Math.random()*15);
+        let buildingHeight=50+level*5+Math.floor(Math.random()*200);        //modifier pour level####
+        buildings[i]={'bdleft':i*widthOfBld,'bdHeight':buildingHeight,'bdCol':buildingCol[colorIndex]}
+    }
+    drawBuildings(buildings,widthOfBld);
+}
+
+
+function findHighest(buildings){               //récupération de l'index du building le plus haut
+
+    let bdList=buildings.map(bd => bd.bdHeight);
+    bdList=bdList.sort((function(a, b){return b - a}));
+    let highIndex=buildings.findIndex(bd => bd.bdHeight == bdList[0]);
+    console.log('high',highIndex);
+    return highIndex;
+}
+
+function drawBuildings(buildings,widthOfBld){    //display du DOM -création de buildings
+    for(let i=0;i<buildings.length;i++){
+        if (document.getElementById(`bd_${i}`)==undefined){
+            let divBldg =document.createElement('div');
+            divBldg.setAttribute('class','building');
+            divBldg.setAttribute('id',`bd_${i}`);
+            divBldg.setAttribute('style',"");
+            DOMFenetre.appendChild(divBldg);}
+        let divBldgIndex=document.getElementById(`bd_${i}`);
+        divBldgIndex.removeAttribute('style');
+        if (buildings[i].bdHeight>0){
+            divBldgIndex.style.height=`${buildings[i].bdHeight}px`;
+            divBldgIndex.innerText=(i+1).toString();}
+        divBldgIndex.style.left=`${i*widthOfBld}%`;
+        divBldgIndex.style.width=`${widthOfBld}%`;
+        divBldgIndex.style.backgroundColor=buildings[i].bdCol;
+        divBldgIndex.style.backgroundImage=`url(../img/windows.png)`;
+    }
+}
+
+function explodeBuilding(bdIndex){        //destruction building par la bombe (hauteur réduite)
+    if (bdIndex==findHighest(buildings)){score+=25;}
+    else{score +=10;}
+    buildings[bdIndex].bdHeight-=Math.floor(300*cheatOn+(40-.3*level));   //#####cheat mode : rase tout #####
+    if (buildings[bdIndex].bdHeight<=0){
+        buildings[bdIndex].bdHeight=0;
+        document.getElementById(`bd_${bdIndex}`).style.border='none';}
+    kaboom(bdIndex);
+    
+    if (hiScore<=score){hiScore=score}
+    DOMBomb.innerHTML="";               //suppression de la bombe
+}
--- js/game.js
+++ js/game.js
@@ -1,10 +1,11 @@
+let cheatOn=1;
 let level=1;
 let score=0;
 let hiScore=0;
 let altitude=540;
 let gameLoop=300;         // intervalle loop
 let planeX=-1;            //position en X de l'avion
-let nbBomb=0;
+let nbBomb=-1;
 let bombX=0;              //position en X de la bombe
 let bombAltitude=550;
 let planeSpeed=3;
@@ -18,37 +19,16 @@ const DOMAim=document.getElementById('aim');
 const DOMAvion=document.getElementById('avion');        //la div en position absolute de l'avion
 const DOMBomb=document.getElementById('bomb');          //la div en position absolute de la bombe
 const DOMFenetre=document.getElementById('fenetre');    //la zone de jeu
-let dropingBomb=document.addEventListener('keyup', ev => {
-    if (ev.code === 'Space') {dropBomb(altitude,planeX)}
+let keyListen=document.addEventListener('keyup', ev => {
+    if (ev.code === 'Space' && nbBomb!=-1) {dropBomb(altitude,planeX)}
+    if (ev.code === 'Space' && nbBomb==-1) {
+        document.getElementById('startMessage').innerText="";
+        mainGame();}
   });
 
 
-
-const buildingCol=['#0000AA','#00AA00','#00AAAA','#AA0000','#AA0000','#AA00AA','#AA5500','#FFFF55',
-                   '#AAAAAA','#555555','#5555FF','#55FF55','#55FFFF','#FF5555','#FF55FF','#FFFFFF'];
-/* 00.loBlack =#000000 ; 01.lowBlue =#0000AA ; 02.lowGren =#00AA00 ;
-   03.lowCyan =#00AAAA ; 04.low_Red =#AA0000 ; 05.lowMagt =#AA00AA ;
-   06.lowYell =#AA5500 ; 07.lowGrey =#AAAAAA ; 08.higGrey =#555555 ;
-   09.higBlue =#5555FF ; 10.higGren =#55FF55 ; 11.higCyan =#55FFFF ;
-   12.hig_Red =#FF5555 ; 13.higMagt =#FF55FF ; 14.higYell =#FFFF55 ;*/
-
-
-function initBuildings(level,buildings){       //création aléatoire des buildings (hauteur et couleur)
-    let numberOfBld = 6+3*level;
-    let widthOfBld = (99/(numberOfBld));
-    for (let i=0; i<numberOfBld; i++){
-        let colorIndex=Math.floor(Math.random()*15);
-        let buildingHeight=50+level*3+Math.floor(Math.random()*150);        //modifier pour level####
-        buildings[i]={'bdleft':i*widthOfBld,'bdHeight':buildingHeight,'bdCol':buildingCol[colorIndex]}
-    }
-    drawBuildings(buildings,widthOfBld);
-}
-async function doNothing(tps){
-    await timer(tps);
-}
-
 async function ready(){                              //attente du lancement
-    planeSpeed=Math.floor(3+level*.3);
+    planeSpeed=Math.floor(3+level*.2);
     affichScore();
     if (document.getElementById('startMessage')==undefined){
        let divStartMssg=document.createElement('div');
@@ -56,45 +36,16 @@ async function ready(){                              //attente du lancement
        DOMFenetre.appendChild(divStartMssg);
     }
     let divStMssg=document.getElementById('startMessage');
-    divStMssg.innerText=`LEVEL ${level} : PRESS ENTER TO START`;
-    const letsStart=document.addEventListener('keyup',ev => {
-        if (ev.code != 'Enter') {doNothing(10)}
-        else {divStMssg.innerText="";
-            mainGame();}
-    });
-}
-function findHighest(buildings){               //récupération de l'index du building le plus haut
-
-    let bdList=buildings.map(bd => bd.bdHeight);
-    bdList=bdList.sort((function(a, b){return b - a}));
-    let highIndex=buildings.findIndex(bd => bd.bdHeight == bdList[0]);
-    console.log('high',highIndex);
-    return highIndex;
+    divStMssg.innerText=`LEVEL ${level} : PRESS <SPACE> TO START`;
+    
 }
 
-function drawBuildings(buildings,widthOfBld){    //display du DOM -création de buildings
-    for(let i=0;i<buildings.length;i++){
-        if (document.getElementById(`bd_${i}`)==undefined){
-            let divBldg =document.createElement('div');
-            divBldg.setAttribute('class','building');
-            divBldg.setAttribute('id',`bd_${i}`);
-            divBldg.setAttribute('style',"");
-            DOMFenetre.appendChild(divBldg);}
-        let divBldgIndex=document.getElementById(`bd_${i}`);
-        divBldgIndex.removeAttribute('style');
-        if (buildings[i].bdHeight>0){divBldgIndex.style.height=`${buildings[i].bdHeight}px`;}
-        divBldgIndex.style.left=`${i*widthOfBld}%`;
-        divBldgIndex.style.width=`${widthOfBld}%`;
-        divBldgIndex.style.backgroundColor=buildings[i].bdCol;
-        divBldgIndex.style.backgroundImage=`url(../img/windows.png)`;
-    }
-}
 
 function dropBomb(alti,lefti){                 //lancement bombe si aucune bombe déjà en cours, selon position avion
     nbBomb+=1;
-    if (DOMBomb.innerHTML=="" && nbBomb>1){
+    if (DOMBomb.innerHTML=="" && nbBomb>=1){
     bombAltitude=alti;
-    bombX=lefti;
+    bombX=lefti-.1;
     if(bombX<1){bombX=1}
     DOMBomb.style.bottom=`${bombAltitude}px`;
     DOMBomb.style.left=`${bombX}%`;
@@ -107,7 +58,7 @@ function affichScore(){
     DOMHiScore.innerText="*High "+hiScore.toString().padStart(5,0);
     DOMAlti.innerText="Altitude "+(altitude*10).toString().padStart(5,0);
     DOMAim.innerText="Aim Bld#"+(findHighest(buildings)+1).toString();
-    DOMAim.style.color=buildings[findHighest(buildings)].bdCol;
+    DOMAim.style.backgroundColor=buildings[findHighest(buildings)].bdCol;
 }
 
 const timer = ms => new Promise(res => setTimeout(res, ms))
@@ -126,20 +77,8 @@ async function kaboom(myIndex){               //effet de destruction building pa
 
 }
 
-function explodeBuilding(bdIndex){        //destruction building par la bombe (hauteur réduite)
-    buildings[bdIndex].bdHeight-=Math.floor(300-.3*level);   //#####cheat mode : modif 40/300 #####
-    if (buildings[bdIndex].bdHeight<=0){
-        buildings[bdIndex].bdHeight=0;
-        document.getElementById(`bd_${bdIndex}`).style.border='none';}
-    kaboom(bdIndex);
-    if (bdIndex==findHighest(buildings)){score+=25;}
-    else{score +=10;}
-    if (hiScore<=score){hiScore=score}
-    DOMBomb.innerHTML="";               //suppression de la bombe
-}
-
 function moveBomb(bombAlti){            //descente de la bombe -contrôle du building dessous
-    let bdIndex=Math.floor(bombX/(99/(6+3*level)));
+    let bdIndex=Math.floor(bombX/(99/(6+2*level)));
     if (bdIndex<0){bdIndex=0}
     if (bombAlti>buildings[bdIndex].bdHeight && DOMBomb.innerHTML!="" && bombAlti>0){
         DOMBomb.style.bottom=`${bombAlti}px`;
@@ -168,7 +107,6 @@ function crashPlane(){               //altitude avion <altitude batiment
     DOMAvion.style.left=`${crashIndex}%`;
     DOMAvion.innerHTML=`<p>YOU LOST</p><img src='img/kaboom.gif' height='90px' width='auto'>`;
     console.log('crash at',altitude,findHighest(buildings));
-    endGame();
 }
 
 async function endGame(){                         //fin de partie, sortie de boucle################
@@ -180,21 +118,20 @@ async function endGame(){                         //fin de partie, sortie de bou
     
 }
 
-async function newLevel(){
+function newLevel(){
     if(altitude==-1){
         planeX=-2;
         level+=1;
         altitude=550-level;
-        nbBomb =0;
+        nbBomb =-1;
         bombAltitude=550;
         buildings=[];
         DOMAvion.innerHTML="";
         initBuildings(level,buildings);
-        document.addEventListener('keyup', ev => {
+       /* document.addEventListener('keyup', ev => {
             if (ev.code === 'Space') {dropBomb(altitude,planeX)}
-            });
+        });*/
     }
-    await timer(500);
     ready();
 }
 
@@ -202,26 +139,31 @@ async function winRound(){                    //passage au level suivant, retour
     //await timer(500);
     DOMAvion.style.bottom=`0px`;
     DOMAvion.innerHTML=`<p>SAFE LANDING</p><img src='img/spritePlane.gif' height='auto' width='90px'>`;
+    document.removeEventListener('keyup', ev =>{});
+    
     for(let landing=0;landing<=80;landing+=5){
         DOMAvion.style.left=`${landing}%`;
-        await timer(Math.ceil(50+3*landing));}
-    await timer(2000);    
-    await newLevel();   
+        await timer(Math.ceil(50+3*landing));
+    }
+    setTimeout(newLevel(),3000);
+    
+    
 }
 
 
 function mainGame(){            //boucle principale
-    
+    nbBomb=0;
     gameLoop= setInterval(function() {
         movePlane(altitude,planeX,bombAltitude);
         planeX +=planeSpeed;
         if (bombAltitude<550){bombAltitude-=40};
         if (planeX>=98){planeX=-1;altitude -=25;}
         if(altitude+10 <buildings[findHighest(buildings)].bdHeight){
+            nbBomb=-1;
             crashPlane();
             endGame();}
         if (buildings[findHighest(buildings)].bdHeight==0){
-                clearInterval(gameLoop); altitude=-1; winRound();}
+                clearInterval(gameLoop); altitude=-1; setTimeout(winRound(),200);}
         }, 300);
 }  
 
